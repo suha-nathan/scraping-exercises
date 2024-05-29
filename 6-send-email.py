aabe3566b2a8a001813f1e7b3be05a22fe868638
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-05-29 17:08:42 -0400

    6 degrees separation

--- 6-send-email.py
+++ 6-send-email.py
@@ -3,8 +3,8 @@ from email.mime.text import MIMEText
 
 msg = MIMEText('The body of the email is here')
 msg['Subject'] = 'An Email Alert'
-msg['From'] = 'suhasini_95@hotmail.com'
-msg['To'] = 'suha.nathan@gmail.com'
+msg['From'] = 'fromemail@hotmail.com'
+msg['To'] = 'test@gmail.com'
 
 s = smtplib.SMTP('localhost')
 s.send_message(msg)
